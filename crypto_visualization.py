65e533819544e224a392db2a442a23a60cdae216
zaiba19 <zaibaiqbal1@gmail.com>
2021-05-07 23:23:01 -0400

    extracted information from request to create tuple

--- crypto_visualization.py
+++ crypto_visualization.py
@@ -3,8 +3,13 @@ import pandas as pd
 
 r = requests.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')  #to timeout on request 
 r_data = r.json()
-id = []
-for i in range(len(r_data)):
-    id.append(r_data[i]["id"])
+# id = []
+data_tuple = [()]
+# for i in range(len(r_data)):
+#     id.append(r_data[i]["id"])
 
-print(id)
+for i in range(len(r_data)):
+    data_tuple.append((r_data[i]["id"],r_data[i]["current_price"],r_data[i]["market_cap"]))
+data_tuple = tuple(data_tuple)
+
+print(data_tuple)
