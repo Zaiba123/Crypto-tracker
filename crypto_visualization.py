ba9ea4120baef4c8b49d03835987f176b19e3a42
zaiba19 <zaibaiqbal1@gmail.com>
2021-05-08 00:40:06 -0400

    added image links to dataframe

--- crypto_visualization.py
+++ crypto_visualization.py
@@ -8,11 +8,9 @@ from IPython.core.display import HTML
 
 r = requests.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')  #to timeout on request 
 r_data = r.json()
-# id = []
+
 data_list = []
 image_list = []
-# for i in range(len(r_data)):
-#     id.append(r_data[i]["id"])
 
 for i in range(len(r_data)):
     data_list.append((r_data[i]["id"],r_data[i]["current_price"],r_data[i]["market_cap"]))
@@ -21,12 +19,7 @@ for i in range(len(r_data)):
 for i in range(len(r_data)):
     image_list.append((r_data[i]["image"]))
 
-# data_tuple = tuple(data_tuple)
-#image_list = tuple(image_list)
-#print(image_list)
-
-
 df = DataFrame (data_list,columns=['id','current price','market cap'])
+df['Images'] = image_list
 print (df)
 
-# print(data_tuple)
